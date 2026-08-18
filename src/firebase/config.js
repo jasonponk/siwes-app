533d81fcc6f493e21427fdf56af5b8bedc1e09f8
jasonponk <jaziemikkie@gmail.com>
2026-08-18 18:25:08 +0100

    Update Firebase configuration details

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,14 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlryK37814aGGnGSn4gP3eaN6zOHvM7fI",
-  authDomain: "cyber-sme-8aee0.firebaseapp.com",
-  databaseURL: "https://cyber-sme-8aee0-default-rtdb.firebaseio.com",
-  projectId: "cyber-sme-8aee0",
-  storageBucket: "cyber-sme-8aee0.firebasestorage.app",
-  messagingSenderId: "315767750143",
-  appId: "1:315767750143:web:bcdc3c2dd955385a88fbca"
+  apiKey: "AIzaSyD3kN_nLzV16uh-iI_hx16GIUxCW01D_H8",
+  authDomain: "siwes-portal.firebaseapp.com",
+  databaseURL: "https://siwes-portal-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "siwes-portal",
+  storageBucket: "siwes-portal.firebasestorage.app",
+  messagingSenderId: "909189034140",
+  appId: "1:909189034140:web:8a4f7e2ecde962026cbd59",
+  measurementId: "G-7QYHPX422V"
 };
 
 const app = initializeApp(firebaseConfig)
